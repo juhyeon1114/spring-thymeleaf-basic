e679d8644bb9d50a1ef36fe5c68bf706b881d04d
Kai Kim <juhyeon1114@gmail.com>
2022-11-22 13:16:59 +0900

    log : 타임리프 기본기능

---
+++ src/test/java/hello/springthymeleafbasic/SpringThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springthymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
